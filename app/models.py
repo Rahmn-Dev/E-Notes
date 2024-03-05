0d57aee5d81e6c118e95c938e40df25879a01bb8
Macbook Aul <sidqi.rahman@gmail.com>
2024-03-05 21:42:46 +0700

    updating models

--- app/models.py
+++ app/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+import uuid
 from django.contrib.auth.models import User
 from django_bleach.models import BleachField
 
 class Workspace(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=100)
     members = models.ManyToManyField(User, related_name='workspaces')
     def __str__(self):
         return self.name
 
 class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     title = models.CharField(max_length=100)
     content = BleachField()
     created_at = models.DateTimeField(auto_now_add=True)
